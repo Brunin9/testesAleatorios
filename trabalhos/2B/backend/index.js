1a1ff59d11aed6b7dc19b25499d27964220d4654
Brunin9 <brubor2008@gmail.com>
2025-07-22 21:18:57 -0300

    testando

--- trabalhos/2B/backend/index.js
+++ trabalhos/2B/backend/index.js
@@ -1,20 +1,18 @@
-const express = require('express'); //framework web do node.js
-const mysql = require('mysql2/promise'); //biblioteca pra usa o Mysql
-const cors = require('cors'); //libera acesso de outros dominios como o frontend
+const express = require('express');
+const mysql = require('mysql2/promise');
+const cors = require('cors');
 
 const app = express();
-app.use(cors()); //isso deixa o frontend acessar o backend
-app.use(express.json()); //isso deixa receber dado em json tipo o titulo
+app.use(cors());
+app.use(express.json());
 
-//em vez de ficar abrindo e fechando conexoes mantem algumas conexcoes prontas pra usar ja 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'db', //diz onde ta o bd de existir DB_HOST no .env usa ela senao usa 'db' q é o nome do servico no compose
-  user: process.env.DB_USER || 'Bruno', //usa do env ou root como padrao
-  password: process.env.DB_PASSWORD || 'Bruno123', //msm coisa 
-  database: process.env.DB_NAME || 'registro_db', //é o banco que se conecta e como padrao é o 'registro_db'
+  host: process.env.DB_HOST || 'db',
+  user: process.env.DB_USER || 'Bruno',
+  password: process.env.DB_PASSWORD || 'Bruno123',
+  database: process.env.DB_NAME || 'registro_db',
 });
 
-//garante q existe a tabela 
 (async () => {
   const conn = await pool.getConnection();
   await conn.query(`
@@ -27,7 +25,6 @@ const pool = mysql.createPool({
   conn.release();
 })();
 
-//rota pra um novo registro com horario atual
 app.post('/registrar', async (req, res) => {
   const { titulo, horario } = req.body;
   if (!titulo || !horario) {
@@ -37,11 +34,10 @@ app.post('/registrar', async (req, res) => {
   res.json({ status: 'ok' });
 });
 
-//rota para listar todos os registros já salvos
 app.get('/registros', async (req, res) => {
-  const [dados] = await pool.query('SELECT * FROM registros ORDER BY id DESC'); //comando pra listar as coisas do bd
-  res.json(dados); //dados contem os registros buscados no bd ent ele pega esses dados e manda como resposta
+  const [dados] = await pool.query('SELECT * FROM registros ORDER BY id DESC');
+  res.json(dados);
 });
 
-const PORT = 3000; //porta q o backend recebe as req
-app.listen(PORT, () => console.log(`Backend rodando na porta ${PORT}`)); //server comeca a escutar oq vem da port e fica esperando as req (a segunda parte é so pra testar se ta funcionando dai imprime aquilo no terminal)
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Backend rodando na porta ${PORT}`));
